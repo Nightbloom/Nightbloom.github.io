b979a1229c7aa26c396e0859cc6d40bf0482b568
Siddharth Sharma <siddharthsharma653@gmail.com>
2022-04-04 18:42:54 +0530

    adjustment

--- src/project.js
+++ src/project.js
@@ -3,48 +3,48 @@ let projects = [
     {
         name: "spotify",
         tags: "#javascript, #fullstack, #ui/ux" ,
-        image: "./img/spotifyy.jpg",
+        image: "spotifyy.jpg",
 
     },
     {
         name: "FPS Game",
         tags: "#ui/ux, #backend",
-        image: "./img/FPShooting.jpg",
+        image: "FPShooting.jpg",
     },
     {
         name: "inote",
         tags: "#javascript, #fullstack, #backend, #ui/ux",
-        image: "./img/Note.jpg",
+        image: "Note.jpg",
     },
     {
         name: "backend",
         tags: "#javascript, #fullstack, #backend",
-        image: "./img/noteback.jpg",
+        image: "noteback.jpg",
     },
     {
         name: "portfolio",
         tags: "#javascript, #fullstack, #ui/ux",
-        image: "./img/portfolioo.png",
+        image: "portfolioo.png",
     },
     {
         name: "Zang",
         tags: "#javascript, #fullstack, #ui/ux, #backend",
-        image: "./img/zang.jpg",
+        image: "zang.jpg",
     },
     {
         name: "ad-blocker",
         tags:  "#fullstack, #ui/ux, #backend",
-        image: "./img/adblocker.jpg",
+        image: "adblocker.jpg",
     },
     {
         name: "razorpay",
         tags: "#javascript, #backend",
-        image: "./img/razorpay.jpg",
+        image: "razorpay.jpg",
     },
     {
         name: "api Config",
         tags: "#javascript, #backend",
-        image: "./img/api.png",
+        image: "api.png",
     },
 ]
 
